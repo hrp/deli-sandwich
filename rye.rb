4115e37d6eb72937ef0a2d4ab5c792c451fdcbce
Hiten Parmar <hiten@hbook.local>
2010-06-13 23:34:08 -0700

    gems

--- rye.rb
+++ rye.rb
@@ -1,30 +1,30 @@
 #!/usr/bin/ruby
-# $KCODE = 'u' if RUBY_VERSION < '1.9'
+$KCODE = 'u' if RUBY_VERSION < '1.9'
 
 %w'rubygems sinatra sandwich haml'.each {|r| require r}
 
 # Group delicious bookmarks by date
 
-# before do
-#   content_type :xml, 'charset' => 'utf-8'
-# end
+before do
+  content_type :xml, 'charset' => 'utf-8'
+end
 
-# get '/:user' do
-#   params[:user] ||= 'madh'
-#   @user = params[:user]
-#   reuben = Sandwich.new(params[:user])
-#   @p = reuben.make_sandwich
-#   @p.each do |d|
-#     d[:title_date] = d[:date].strftime('%B %d, %Y').gsub(/\s0/, ' ')
-#     d[:desc] = '<ul>'
-#     d[:bookmarks].each do |bm|
-#       d[:desc] << %[<li><a href='#{bm[:url]}'>#{bm[:title]}</a></li>]
-#     end
-#     d[:desc] << '</ul>'
-#     d[:desc] = h d[:desc]
-#   end
-#   haml :rss
-# end
+get '/:user' do
+  params[:user] ||= 'madh'
+  @user = params[:user]
+  reuben = Sandwich.new(params[:user])
+  @p = reuben.make_sandwich
+  @p.each do |d|
+    d[:title_date] = d[:date].strftime('%B %d, %Y').gsub(/\s0/, ' ')
+    d[:desc] = '<ul>'
+    d[:bookmarks].each do |bm|
+      d[:desc] << %[<li><a href='#{bm[:url]}'>#{bm[:title]}</a></li>]
+    end
+    d[:desc] << '</ul>'
+    d[:desc] = h d[:desc]
+  end
+  haml :rss
+end
 
 get '/' do
   # "Welcome to the deli. We're currently serving delicious links each day to tumblr."

7a80a1254199b8350aa4e583f6ff9b3e07059891
Hiten Parmar <hiten@hbook.local>
2010-06-13 22:16:23 -0700

    valid xml

--- rye.rb
+++ rye.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 $KCODE = 'u' if RUBY_VERSION < '1.9'
+
 %w'rubygems sinatra sandwich haml'.each {|r| require r}
 
 # Group delicious bookmarks by date
@@ -15,10 +16,21 @@ get '/:user' do
   @p = reuben.make_sandwich
   @p.each do |d|
     d[:title_date] = d[:date].strftime('%B %d, %Y').gsub(/\s0/, ' ')
+    d[:desc] = '<ul>'
+    d[:bookmarks].each do |bm|
+      d[:desc] << %[<li><a href='#{bm[:url]}'>#{bm[:title]}</a></li>]
+    end
+    d[:desc] << '</ul>'
+    d[:desc] = h d[:desc]
   end
   haml :rss
 end
 
 get '/' do
   "Welcome to the deli. We're currently serving delicious links each day to tumblr."
+end
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
 end
